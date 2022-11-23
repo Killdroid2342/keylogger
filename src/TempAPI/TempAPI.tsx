67294c69abcd5384b572776b381d29a5da26d6cc
Killdroid2342 <113387485+Killdroid2342@users.noreply.github.com>
2022-11-23 21:53:42 +0000

    added title and styled title

--- src/TempAPI/TempAPI.tsx
+++ src/TempAPI/TempAPI.tsx
@@ -2,17 +2,19 @@ import React from 'react';
 
 const TempAPI = () => {
     return (
-        <div className='flex flex-col items-center justify-center my-auto h-screen text-center'>
-
-        
-        <div className=' border-2 border-white flex flex-col rounded-md p-12 '>
-          <h1 className='pb-8 text-2xl text-white'>Current Tempreture</h1>
-        <div>
-            <p className='text-l font-sans'>current tempreture is 0</p>
+        <>
+            <h1 className='text-center text-3xl font-bold text-white mt-40'>Recorded in °C</h1>
+            <div className='flex flex-col items-center justify-center  mt-40 text-center'>
+            
+            <div className=' border-2 border-white flex flex-col rounded-md p-12 '>
+            <h2 className='pb-8 text-2xl text-white font-bold'>Current Tempreture</h2>
+            <div>
+                <p className='text-l font-sans'>current tempreture is 0</p>
+            </div>
+            
+            </div>
         </div>
-         
-        </div>
-      </div>
+        </>
     );
 };
 
