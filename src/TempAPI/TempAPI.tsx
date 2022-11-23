af15bac5bdc6ef554e4025ca1c11ec2e4de59227
Killdroid2342 <113387485+Killdroid2342@users.noreply.github.com>
2022-11-23 21:47:17 +0000

    removed prev jsx

--- src/TempAPI/TempAPI.tsx
+++ src/TempAPI/TempAPI.tsx
@@ -2,9 +2,7 @@ import React from 'react';
 
 const TempAPI = () => {
     return (
-        <div className='bg-white border-2 border-red-900'>
-            test
-        </div>
+        
     );
 };
 
