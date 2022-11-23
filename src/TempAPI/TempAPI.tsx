ad09e939e0c165148870b25a3a3063ce03c45012
Killdroid2342 <113387485+Killdroid2342@users.noreply.github.com>
2022-11-23 21:47:34 +0000

    added new jsx

--- src/TempAPI/TempAPI.tsx
+++ src/TempAPI/TempAPI.tsx
@@ -2,7 +2,17 @@ import React from 'react';
 
 const TempAPI = () => {
     return (
+        <div className='flex flex-col items-center justify-center my-auto h-screen text-center'>
+
         
+        <div className=' border-2 border-white flex flex-col rounded-md p-12 '>
+          <h1 className='pb-8 text-2xl text-white'>Current Tempreture</h1>
+        <div>
+            <p className='text-l font-sans'>current tempreture is 0</p>
+        </div>
+         
+        </div>
+      </div>
     );
 };
 
