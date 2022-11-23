ae4cbb6e7d1a0f8ff885fd0b0e7ae52ac36aad70
Killdroid2342 <113387485+Killdroid2342@users.noreply.github.com>
2022-11-23 21:57:10 +0000

    fixed spacing and added a few more styles

--- src/TempAPI/TempAPI.tsx
+++ src/TempAPI/TempAPI.tsx
@@ -4,13 +4,13 @@ const TempAPI = () => {
     return (
         <>
             <h1 className='text-center text-3xl font-bold text-white mt-40'>Recorded in °C</h1>
-            <div className='flex flex-col items-center justify-center  mt-40 text-center'>
+            <div className='flex flex-col items-center justify-center  mt-28 text-center'>
             
             <div className=' border-2 border-white flex flex-col rounded-md p-12 '>
-            <h2 className='pb-8 text-2xl text-white font-bold'>Current Tempreture</h2>
-            <div>
-                <p className='text-l font-sans'>current tempreture is 0</p>
-            </div>
+                <h2 className='pb-8 text-2xl text-white font-bold'>Current Tempreture</h2>
+                <div>
+                    <p className='text-l font-sans border-2 font-bold border-white p-4 rounded-md text-white'>current tempreture is 0°C</p>
+                </div>
             
             </div>
         </div>
