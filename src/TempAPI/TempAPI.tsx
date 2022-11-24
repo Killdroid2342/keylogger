c4cfef9af6015501f1944a590aac846c4e033bf0
Killdroid2342 <113387485+Killdroid2342@users.noreply.github.com>
2022-11-24 00:21:00 +0000

    re done all the logic and made it cleaner

--- src/TempAPI/TempAPI.tsx
+++ src/TempAPI/TempAPI.tsx
@@ -3,18 +3,14 @@ import React from 'react';
 const TempAPI = () => {
     return (
         <>
-            <h1 className='text-center text-3xl font-bold text-white mt-40'>Recorded in °C</h1>
-            <div className='flex flex-col items-center justify-center  mt-28 text-center'>
-            
-            <div className=' border-2 border-white flex flex-col rounded-md p-12 '>
-                <h2 className='pb-8 text-2xl text-white font-bold'>Current Tempreture</h2>
-                <div>
-                    <p className='text-l font-sans border-2 font-bold border-white p-4 rounded-md text-white'>current tempreture is 0°C</p>
-                    
+            <h1 className='text-center text-3xl font-bold text-white mt-40'>Temperature App</h1>
+            <div className='flex flex-col items-center justify-center mt-28 text-center'>
+                <div className=' border-2 border-white flex flex-col rounded-md p-12 '>
+                    <h2 className='pb-8 text-2xl text-white font-bold'>Location: London</h2>
+                    <p className='text-l font-sans border-2 font-bold border-white p-4 rounded-md text-white'>Current Temperature Is 0°C</p>
+                  
                 </div>
-
             </div>
-        </div>
         </>
     );
 };
