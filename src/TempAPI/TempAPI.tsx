d455571d9058394a4d6a9131b20c183cfdca46b2
Killdroid2342 <113387485+Killdroid2342@users.noreply.github.com>
2022-11-25 01:14:34 +0000

    added place for data

--- src/TempAPI/TempAPI.tsx
+++ src/TempAPI/TempAPI.tsx
@@ -22,7 +22,7 @@ const TempAPI = () => {
             <div className='flex flex-col items-center justify-center mt-28 text-center'>
                 <div className=' border-2 border-white flex flex-col rounded-md p-12 '>
                     <h2 className='pb-8 text-2xl text-white font-bold'>Location: London</h2>
-                    <p className='text-l font-sans border-2 font-bold border-white p-4 rounded-md text-white'>Current Temperature Is 0°C</p>
+                    <p className='text-l font-sans border-2 font-bold border-white p-4 rounded-md text-white'>Current Temperature Is 0°C {weather}</p>
                     <button className='border-2 border-white p-3 rounded-md text-l font-bold text-white mt-8' onClick={getWeather}>Check Temperature In Your Location</button>
                 </div>
             </div>
