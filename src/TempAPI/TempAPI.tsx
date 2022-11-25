d84d3cd6ee9cfb80f1db3285b6ebb2ff673350f8
Killdroid2342 <113387485+Killdroid2342@users.noreply.github.com>
2022-11-25 00:43:39 +0000

    added onClick

--- src/TempAPI/TempAPI.tsx
+++ src/TempAPI/TempAPI.tsx
@@ -23,7 +23,7 @@ const TempAPI = () => {
                 <div className=' border-2 border-white flex flex-col rounded-md p-12 '>
                     <h2 className='pb-8 text-2xl text-white font-bold'>Location: London</h2>
                     <p className='text-l font-sans border-2 font-bold border-white p-4 rounded-md text-white'>Current Temperature Is 0°C</p>
-                    <button className='border-2 border-white p-3 rounded-md text-l font-bold text-white mt-8'>Get Temperature And Location</button>
+                    <button className='border-2 border-white p-3 rounded-md text-l font-bold text-white mt-8' onClick={getWeather}>Get Temperature And Location</button>
                 </div>
             </div>
         </Fragment>
