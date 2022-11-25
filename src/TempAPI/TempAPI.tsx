f74cf44fd294f6095444d1dc38f21928fb814cda
Killdroid2342 <113387485+Killdroid2342@users.noreply.github.com>
2022-11-25 01:13:35 +0000

    added api NOT Finished

--- src/TempAPI/TempAPI.tsx
+++ src/TempAPI/TempAPI.tsx
@@ -6,10 +6,10 @@ const TempAPI = () => {
     const getWeather = async () => {
         axios
           .get(
-            ''
-          )
+            'http://api.openweathermap.org/data/2.5/forecast?id=524901&appid={6281aa4868ce6489c8799b5111a227c8}'
+            )
           .then((res) => {
-            // console.log(res);
+            console.log(res);
             setWeather(res.data);
           })
           .catch((err) => {
@@ -23,7 +23,7 @@ const TempAPI = () => {
                 <div className=' border-2 border-white flex flex-col rounded-md p-12 '>
                     <h2 className='pb-8 text-2xl text-white font-bold'>Location: London</h2>
                     <p className='text-l font-sans border-2 font-bold border-white p-4 rounded-md text-white'>Current Temperature Is 0°C</p>
-                    <button className='border-2 border-white p-3 rounded-md text-l font-bold text-white mt-8' onClick={getWeather}>Get Temperature And Location</button>
+                    <button className='border-2 border-white p-3 rounded-md text-l font-bold text-white mt-8' onClick={getWeather}>Check Temperature In Your Location</button>
                 </div>
             </div>
         </Fragment>
