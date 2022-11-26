99bc13bc6d836c726ea6d1dc0fa91eb0bbfd128f
Killdroid2342 <113387485+Killdroid2342@users.noreply.github.com>
2022-11-26 18:37:58 +0000

    styling input

--- src/See-Typing/See-Typing.tsx
+++ src/See-Typing/See-Typing.tsx
@@ -3,12 +3,12 @@ import React, { Fragment, useState } from 'react';
 const SeeType = () => {
   const [typing, setTyping] = useState('')
   const type = () => {
-    
+
   }
    return(
     <Fragment>
       <div>
-        <input type="text" onChange={type} />
+        <input type="text" onChange={type} placeholder="Enter Text Here" className='border-2 border-white bg-transparent text-white rounded-md p-2 font-bold font-mono'/>
       </div>
     </Fragment>
    )
