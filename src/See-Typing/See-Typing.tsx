35e7d0d9fadddd50087ba809151401a71495f6b1
Killdroid2342 <113387485+Killdroid2342@users.noreply.github.com>
2022-11-26 18:51:43 +0000

    removed fragment added new styling

--- src/See-Typing/See-Typing.tsx
+++ src/See-Typing/See-Typing.tsx
@@ -1,16 +1,17 @@
-import React, { Fragment, useState } from 'react';
+import React, { useState } from 'react';
 
 const SeeType = () => {
   const [typing, setTyping] = useState('')
   const type = () => {
-
+    
   }
    return(
-    <Fragment>
-      <div>
-        <input type="text" onChange={type} placeholder="Enter Text Here" className='border-2 border-white bg-transparent text-white rounded-md p-2 font-bold font-mono'/>
-      </div>
-    </Fragment>
+    <div className='flex flex-col items-center justify-center'>
+      <h1 className=''>See What You Type</h1>
+      <input type="text" onChange={type} placeholder="Enter Text Here" className='border-2 border-white bg-transparent text-white rounded-md text-center p-3 font-bold font-mono'/>
+      <p>{typing}asdda</p>
+    </div>
+
    )
 };
 
