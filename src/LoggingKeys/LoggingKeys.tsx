45c67ca79f3add791dd6a2efd18b08f1bc28535f
Killdroid2342 <113387485+Killdroid2342@users.noreply.github.com>
2022-11-28 02:19:43 +0000

    deleted console log and made text bigger

--- src/LoggingKeys/LoggingKeys.tsx
+++ src/LoggingKeys/LoggingKeys.tsx
@@ -5,17 +5,16 @@ const SeeType = () => {
 
   const type = (e: React.ChangeEvent<HTMLInputElement>) => {
     setKeys(e.target.value);
-    console.log(keys);
   };
   return (
     <div className='flex flex-col items-center justify-center h-screen'>
       <div className='m-auto'>
-        <h1 className='mx-auto font-serif font-bold w-fit text-white mb-10 my-2 text-xl'>
+        <h1 className='mx-auto font-serif font-bold w-fit text-white mb-10 my-2 text-2xl'>
           Log Your Keys
         </h1>
         <input
           type='text'
-          onChange={type}
+          onChange={(e) => type(e)}
           placeholder='Enter Text Here'
           className='border-2 border-white bg-transparent text-white w-fit placeholder-white rounded-md text-center p-3 font-bold font-mono'
         />
